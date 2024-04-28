89b4e7d5f26c3c4cb5761a965df3ec599ff7fe00
Sruti <sgsofficial.system@gmail.com>
2024-04-27 22:06:38 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 10;
+
+function checkGuess() {
+  attempts--;
+  const inputElement = document.getElementById("guess");
+  const feedbackElement = document.getElementById("feedback");
+  const guess = inputElement.value;
+  while (attempts > 0) {
+    if (guess == randomNumber) {
+      attempts = 0;
+      feedbackElement.innerHTML = "Congratulations! " + attempts + " left";
+      feedbackElement.style.color = "green";
+      break;
+    } else if (guess < randomNumber) {
+      feedbackElement.innerHTML = "Too low! Try again. " + attempts + " left";
+      feedbackElement.style.color = "red";
+      break;
+    } else {
+      feedbackElement.innerHTML = "Too high! Try again. " + attempts + " left";
+      feedbackElement.style.color = "red";
+      break;
+    }
+  }
+  if (attempts === 0 && guess !== randomNumber){
+    feedbackElement.style.color = "red";
+    feedbackElement.innerHTML = "Game over"
+  }
+}
